0f46f6ad194cd5f46cb0acd5f2eb3b9ba11ad11f
Andrei Malyshev <mvvandcs@gmail.com>
2022-10-02 17:04:09 +0300

    before prod

--- sweater/bot.py
+++ sweater/bot.py
@@ -1,6 +1,6 @@
 import telebot
 from telebot import types
-from sweater.models import Query
+from sweater.models import Query, Product
 from sweater import db
 from sweater.avito_parser import query_products
 
@@ -23,15 +23,6 @@ def start(message):
                      reply_markup=keyboard)
 
 
-@bot.message_handler(commands=['products'])
-def start_message(message):
-    if message.chat.id not in login_id:
-        bot.send_message(message.chat.id, "Сначала введите пароль.")
-        return
-
-    bot.send_message(message.chat.id, "Вывод...")
-
-
 @bot.message_handler(commands=['new'])
 def new_product_query(message):
     if message.chat.id not in login_id:
@@ -39,6 +30,17 @@ def new_product_query(message):
         return
     send = bot.send_message(message.chat.id, "Введите наименование товара:")
     bot.register_next_step_handler(send, new_product)
+
+
+@bot.message_handler(commands=['products'])
+def start_message(message):
+    if message.chat.id not in login_id:
+        bot.send_message(message.chat.id, "Сначала введите пароль.")
+        return
+    with db.session() as session:
+        products = session.query(Product).all()
+    send = "\n".join(products)
+    bot.send_message(message.chat.id, send)
 
 
 def new_product(message):
